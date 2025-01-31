b7a388c91fa802db0a688dd7ba805cdf1aeb1c6e
Lars Gabriel <lgabriel23@gmx.de>
2025-01-31 07:25:16 +0000

    added preliminary version, hints are not filtered yet

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # EvidencePipeline
+
 EvidencePipeline is a tool for generating evidence for genome annotation using protein and transcriptomic data (RNA-Seq and Iso-Seq reads). It automates alignment, evidence generation, and high-confidence gene selection.
+
+## Installation
+
+### Prerequisites
+Ensure that you have the following dependencies installed:
+- [HISAT2](https://daehwankimlab.github.io/hisat2/)
+- [Minimap2](https://github.com/lh3/minimap2)
+- [StringTie](https://ccb.jhu.edu/software/stringtie/)
+- [SAMtools](http://www.htslib.org/)
+- [TransDecoder](https://github.com/TransDecoder/TransDecoder)
+- [DIAMOND](https://github.com/bbuchfink/diamond)
+- [Bedtools](https://bedtools.readthedocs.io/)
+- [Biopython](https://biopython.org/)
+
+The relevant scripts should be in your PATH or specified as command line arguments.
+
+### Install Python Dependencies
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+### Running EvidencePipeline
+
+To run EvidencePipeline, use the following command:
+```bash
+python evidence_pipeline.py -g <genome.fasta> -p <proteins.fasta> [-sp <paired_reads>] [-ss <single_reads>] [-l <long_reads>] -y <config.yaml> --output_path <output_dir>
+```
+
+### Required Arguments:
+- `-g, --genome` : Path to the genome FASTA file.
+- `-p, --proteins` : Path to the protein FASTA file.
+OR
+- `-y, --config` : Path to a configuration YAML file that includes genome and proteins path. If config and command-line arguments are both provided, command-line arguments will take precedence.
+
+### Optional Arguments:
+- `-sp, --rnaseq_paired` : Comma-separated paired-end RNA-Seq read files.
+- `-ss, --rnaseq_single` : Comma-separated single-end RNA-Seq read files.
+- `-l, --isoseq` : Comma-separated long read files.
+- `--output_path` : Directory where output files will be stored.
+- `--restart` : Resume from previous results if available.
+- `--threads` : Number of threads to use (default: 4).
+
+### Example Run
+```bash
+python evidence_pipeline.py -g genome.fasta -p proteins.fasta -sp rnaseq_1.fq,rnaseq_2.fq -l isoseq.fq -y config.yaml --output_path results/
+```
+
+## Pipeline Overview
+1. **Initialization:**
+   - Reads configuration parameters.
+   - Determines the mode (RNA-Seq, Iso-Seq, or Mixed).
+   - Checks dependencies.
+
+2. **Alignment:**
+   - Align proteins to the genome using Miniprot.
+   - Align RNA-Seq reads using HISAT2.
+   - Align Iso-Seq reads using Minimap2.
+
+3. **Transcript Assembly & ORF Prediction:**
+   - Uses StringTie to assemble transcripts.
+   - Uses TransDecoder to predict open reading frames (ORFs).
+
+4. **Evidence Generation:**
+   - Converts protein and transcript alignments into AUGUSTUS hints.
+   - Generates high-confidence genes.
+
+5. **Output Generation:**
+   - Produces GFF3, FASTA, and other annotation files.
+
+## Output Files
+- `hintsfile.gff` : AUGUSTUS-compatible hints file.
+- `training.gff` : High-confidence genes that can be used for training.
+
+## Logging
+All logs are saved in `LOG.out` inside the output directory.
+
+## License
+This project is licensed under the MIT License.
+
+## Contact
+For questions or issues, please open an issue on the repository or contact the developer.
+
---
+++ bin/evi_initializer.py
@@ -0,0 +1,144 @@
+import argparse, logging, shutil, os
+from utility import read_yaml, find_executable, file_exists_and_not_empty
+
+logger = logging.getLogger(__name__)
+
+def get_config(args):
+    """Read argument and generate a config dictionary"""
+    if args.config:
+        cfg = read_yaml(args.config)
+    else:
+        cfg = {}
+    # Overwrite values in dict with args
+    for key in vars(args):
+        value = getattr(args, key)
+        if value is not None:  # Only overwrite if the argument is provided
+            cfg[key] = value
+            if value:
+                logging.info(f'Setting {key} to {value}.')
+    return cfg
+
+def determine_mode(cfg):
+    """Determine the mode based on the given input data.
+    
+    :cfg: Dictionary containing input configuration data.
+    :return: The determined mode as a string, or raises an error if requirements are not met.
+    """
+    # Define mode requirements
+    mode_requirements = {'mixed' : [['rnaseq_paired', 'rnaseq_single'], ['isoseq', 'proteins']],
+                'isoseq': [[], ['isoseq', 'proteins']], 
+                'rnaseq': [['rnaseq_paired', 'rnaseq_single'], ['proteins']],
+                'proteins': [[], ['proteins']]}
+    for mode, (any_keys, all_keys) in mode_requirements.items():
+        # Check if at least one key from `any_keys` exists and has a non-None value
+        any_valid = not any_keys or any(
+            key in cfg and cfg[key] not in [None, []] for key in any_keys
+        )
+        
+        # Check if all keys from `all_keys` exist and have non-None values
+        all_valid = all(
+            key in cfg and cfg[key] not in [None, []] for key in all_keys
+        )
+
+        # If both conditions are satisfied, return the mode
+        if any_valid and all_valid:
+            logging.info(f'Running in {mode} mode.')
+            return mode
+
+    # If no mode matches, raise an error
+    error_message = (
+        "Input missing! You need to specify at least 'proteins' and either Isoseq or RNA-Seq data."
+    )
+    logging.error(error_message)
+    raise ValueError(error_message)
+
+
+def check_dependencies(cfg):
+    """Try to find required dependencies for mode"""
+    cfg["GALBA_SCRIPTS"] = find_executable(
+                tool_path=cfg['GALBA_SCRIPTS'] if cfg['GALBA_SCRIPTS'] else os.path.dirname(os.path.realpath(__file__)), 
+                executable_name="aln2hints.pl", tool_name='GALBA_SCRIPTS')
+    cfg["DIAMOND"] = find_executable(tool_path=cfg['DIAMOND'], 
+                executable_name="diamond", tool_name='DIAMOND')
+    cfg["BEDTOOLS"] = find_executable(tool_path=cfg['BEDTOOLS'], 
+                executable_name="bedtools", tool_name='BEDTOOLS')
+    cfg["MINIPROT"] = find_executable(tool_path=cfg['MINIPROT'], 
+                executable_name="miniprot", tool_name='MINIPROT')
+    cfg["MINIPROT_HINT"] = find_executable(tool_path=cfg['MINIPROT_HINT'], 
+                executable_name="miniprothint.py", tool_name='MINIPROT_HINT')
+    cfg["MINIPROT_BOUNDARY_SCORER"] = find_executable(tool_path=cfg['MINIPROT_BOUNDARY_SCORER'], 
+                executable_name="miniprot_boundary_scorer", tool_name='MINIPROT_BOUNDARY_SCORER')
+
+    # add default scoring matrix for miniprot-boundary-scorer
+    if not "scoring_matrix" in cfg or cfg["scoring_matrix"] is None:
+        cfg["scoring_matrix"] = cfg["MINIPROT_BOUNDARY_SCORER"] + "/blosum62.csv"
+        if not file_exists_and_not_empty(cfg["scoring_matrix"]):
+            error_message = f"Couldn't find scoring matrix for miniprot-boundary-scorer. It should be located in the directory of miniprot-boundary-scorer or specified as argument."
+            logging.error(error_message)
+            raise ValueError(error_message)
+
+    if cfg["mode"] in ['mixed', 'rnaseq']:
+        cfg["HISAT"] = find_executable(tool_path=cfg['HISAT'], 
+                    executable_name="hisat2", tool_name='HISAT')
+
+    if cfg["mode"] in ['mixed', 'isoseq']:
+        cfg["MINIMAP"] = find_executable(tool_path=cfg['MINIMAP'], 
+                    executable_name="minimap2", tool_name='MINIMAP')
+
+    if cfg["mode"] in ['mixed', 'isoseq', 'rnaseq']:
+        cfg["STRINGTIE"] = find_executable(tool_path=cfg['STRINGTIE'], 
+                    executable_name="stringtie", tool_name='STRINGTIE')
+        cfg["SAMTOOLS"] = find_executable(tool_path=cfg['SAMTOOLS'], 
+                    executable_name="samtools", tool_name='SAMTOOLS')
+        cfg["TRANSDECODER"] = find_executable(tool_path=cfg['TRANSDECODER'], 
+                    executable_name="TransDecoder.LongOrfs", tool_name='TRANSDECODER')
+        cfg["TRANSDECODER_UTIL"] = find_executable(tool_path=f"{cfg['TRANSDECODER']}/util", 
+                    executable_name="gtf_genome_to_cdna_fasta.pl", tool_name='TRANSDECODER')
+        cfg["BAM2HINTS"] = find_executable(tool_path=cfg['BAM2HINTS'], 
+                    executable_name="bam2hints", tool_name='BAM2HINTS')
+    return cfg
+
+
+def setup_logger(logfile=None):
+    logging.basicConfig(
+        level=logging.DEBUG,  # Change to INFO, WARNING, etc., as needed
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(logfile),  # Log to a file
+            logging.StreamHandler(),        # Log to the console
+        ],
+    )
+
+def get_parser():
+    """Define the command-line arguments accepted by the script."""
+    
+    parser = argparse.ArgumentParser(description='Genome annotation with protein and transcriptomic evidence auch as RNA-Seq and Iso-Seq reads')  
+    parser.add_argument('-t', '--threads', default=4, help='Number of threads (default=4)', required=False)
+    parser.add_argument('-y', '--config', help='Config file path', metavar='<config.yaml>', required=False) 
+    parser.add_argument('-g', '--genome', help='Genome file path', metavar='<genome.fasta>', required=False)
+    parser.add_argument('-p', '--proteins', help='Protein file path', metavar='<proteins.fasta>', required=False)
+    parser.add_argument('-sp', '--rnaseq_paired', help='Comma separated paired-end short read file paths', 
+            metavar='<set1.fasta,set2.fasta>', required=False, default=None)
+    parser.add_argument('-ss', '--rnaseq_single', help='Comma separated single-end short read file paths', 
+            metavar='<set1.fasta,set2.fasta>', required=False, default=None)
+    parser.add_argument('-l', '--isoseq', help='Comma separated long read file paths', 
+            metavar='<set1.fasta,set2.fasta>', required=False, default=None)
+    parser.add_argument('-m', '--scoring_matrix', help='Alignment scoring matrix for amino acids', metavar='<blosum62.csv>', required=False)
+    parser.add_argument('--output_path',  default='./pregalba/',
+            help='Specify the path you want the output folder to be created in if it should differ from the current working directory')
+    parser.add_argument('--restart', action='store_true', 
+            help='Use results from a previous run to skip steps. The results have to be located in --output_path.')
+    parser.add_argument('--HISAT', help='Provide the path to the HISAT2 directory', required=False, default='')
+    parser.add_argument('--MINIMAP', help='Provide the path to the Minimap2 directory', required=False, default='')
+    parser.add_argument('--STRINGTIE', help='Provide the path to the StringTie2 directory', required=False, default='')
+    parser.add_argument('--SAMTOOLS', help='Provide the path to the samtools directory', required=False, default='')
+    parser.add_argument('--TRANSDECODER', help='Provide the path to the TransDecoder directory', required=False, default='')
+    parser.add_argument('--DIAMOND', help='Provide the path to the DIAMOND directory', required=False, default='')
+    parser.add_argument('--BEDTOOLS', help='Provide the path to the bedtools2 directory', required=False, default='')
+    parser.add_argument('--MINIPROT', help='Provide the path to the miniprot directory', required=False, default='')
+    parser.add_argument('--MINIPROT_HINT', help='Provide the path to the miniprot_hint directory', required=False, default='')
+    parser.add_argument('--MINIPROT_BOUNDARY_SCORER', help='Provide the path to the miniprot-boundary-scorer directory', required=False, default='')
+    parser.add_argument('--GALBA_SCRIPTS', help='Provide the path to galba scripts like aln2hints.pl directory. Default is the same directory as pregalba.py', required=False, default='')
+    parser.add_argument('--BAM2HINTS', help='Provide the path to bam2hints directory.', required=False, default='')
+    args = parser.parse_args()
+    return args
